9cb4bb63e0af870d8c623e2832c7daa7a711808e
Tyler Finn <tfinn@zvelo.com>
2014-07-21 11:12:35 -0600

    changed settings for timezones and nginx

--- graphite/local_settings.py
+++ graphite/local_settings.py
@@ -4,7 +4,7 @@
 #DEBUG = True
 
 # Set your local timezone (django will try to figure this out automatically)
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 # Setting MEMCACHE_HOSTS to be empty will turn off use of memcached entirely
 #MEMCACHE_HOSTS = ['127.0.0.1:11211']
@@ -12,8 +12,8 @@ TIME_ZONE = 'UTC'
 # Sometimes you need to do a lot of rendering work but cannot share your storage mount
 #REMOTE_RENDERING = True
 #RENDERING_HOSTS = ['fastserver01','fastserver02']
-#LOG_RENDERING_PERFORMANCE = True
-#LOG_CACHE_PERFORMANCE = True
+LOG_RENDERING_PERFORMANCE = True
+LOG_CACHE_PERFORMANCE = True
 
 # If you've got more than one backend server they should all be listed here
 #CLUSTER_SERVERS = []
